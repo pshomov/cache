1599b75e2ea2d737ff1eaccb88cd93f66c9b3b55
pshomov <pshomov@gmail.com>
2009-10-14 13:00:54 +0000

    adding the license note at the top of all code, ready to be released to the public

--- src/main/java/is/siminn/asgard/cache/Cached.java
+++ src/main/java/is/siminn/asgard/cache/Cached.java
@@ -1,5 +1,29 @@
+/*
+
+Created by Petar Shomov <petar@sprettur.is> and contributors
+
+Copyright (c) 2009 Síminn hf (http://www.siminn.is). All rights
+reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Inital version of this file contributed by Síminn hf. (http://www.siminn.is)
+
+*/
 package is.siminn.asgard.cache;
 
-@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME) @java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.TYPE}) public @interface Cached {
+@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.TYPE})
+public @interface Cached {
     boolean enabled() default true;
 }
